ed8c3d6b4bd8b25f51f5f8511d7d9c5fd72dad38
timothybaraka <timothybaraka44@gmail.com>
2020-06-12 11:40:51 +0300

    Add a toast to the onclick listener

--- app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
@@ -3,12 +3,26 @@ package com.moringaschool.myrestaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello world",Toast.LENGTH_LONG).show();
+                //do something
+            }
+            });
     }
-}
+};
